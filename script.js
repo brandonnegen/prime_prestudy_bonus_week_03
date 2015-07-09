4674e9ba107dca565e23f5a617de4d81979ee728
brandonnegen <bnegen@gmail.com>
2015-07-09 13:52:28 -0500

    Fifth Try

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 console.log(x*y*z)
-var x = firstFunction;
-var y = secondFunction;
-var z = thirdFunction;
+var x = firstFunction(2,3);
+var y = secondFunction,(4,5);
+var z = thirdFunction(6,7);
 
-function firstFunction (2,3) {
-	return 2 + 3
+function firstFunction (a,b) {
+	return a + b
 }
-function secondFunction (4,5) {
-	return	4 + 5
+function secondFunction (a,b) {
+	return	a + b
 }
-function thirdFunction (6,7) {
-	return 6 + 7
+function thirdFunction (a,b) {
+	return a + b
 }
