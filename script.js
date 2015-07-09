5d26ee32d4675a3534fb7100f4509c030636b9f7
brandonnegen <bnegen@gmail.com>
2015-07-09 13:47:23 -0500

    add numbers

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ var x = firstFunction(2,3);
 var y = secondFunction(4,5);
 var z = thirdFunction(6,7);
 
-function firstFunction (a,b) {
-	return a + b
+function firstFunction (2,3) {
+	return 2 + 3
 }
-function secondFunction (a,b) {
-	return	a + b
+function secondFunction (4,5) {
+	return	4 + 5
 }
-function thirdFunction (a,b) {
-	return a + b
+function thirdFunction (6,7) {
+	return 6 + 7
 }
 function sumOfFuntions (x,y,z) {
 	return x + y + z
