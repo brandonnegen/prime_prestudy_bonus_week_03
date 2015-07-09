7496e0e68b05e0658044868f83e86f64b1cfb058
brandonnegen <bnegen@gmail.com>
2015-07-09 13:42:07 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var x = firstFunction(2,3);
+var y = secondFunction(4,5);
+var z = thirdFunction(6,7);
+
+function firstFunction (a,b) {
+	return a + b
+}
+function secondFunction (a,b) {
+	return	a + b
+}
+function thirdFunction (a,b) {
+	return a + b
+}
+function sumOfFuntions (x,y,z) {
+	return x + y + z
+}
